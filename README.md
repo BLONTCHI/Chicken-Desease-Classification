8d43490bfc9050ddb8b8e70778b659dad6843488
Benjamin LONTCHI <benjamin.lontchi@groupe-esigelec.org>
2023-11-09 16:48:03 +0100

    :tada: data ingestion component created: Stage_01 created !!

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 
 ## Workflows
 
-1. Update config.yaml
+1. Update configuration.yaml
 2. Update secrets.yaml [Optional]
-3. Update params.yaml
+3. Update params.yaml: params.yaml is the file where will be set hyperparameters for the model
 4. Update the entity
 5. Update the configuration manager in src config
 6. Update the components
---
+++ src/cnnClassifier/components/data_ingestion.py
@@ -0,0 +1,33 @@
+import os
+import zipfile
+from pathlib import Path
+from urllib import request
+from src.cnnClassifier import logger
+from src.cnnClassifier.utils.common import get_size
+from src.cnnClassifier.entity.config_entity import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self, config:DataIngestionConfig):
+        self.config = config
+    
+    
+    def download_data(self):
+        if not os.path.exists(self.config.local_data_filename):
+            filename, headers = request.urlretrieve(url=self.config.source_url,
+                                                    filename=self.config.local_data_filename)
+            logger.info(f"{filename} downloaded with following info: \n{headers}")
+        else:
+            logger.info(f"{filename} already exits of size: {get_size(Path(self.config.local_data_filename))}")
+            
+            
+    def extract_zipfile(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_filename, 'r') as zip_file:
+            zip_file.extractall(unzip_path)
