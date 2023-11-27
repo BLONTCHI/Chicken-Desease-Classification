62c0ad3da2d722b3ed00998268bc51ab65471406
Benjamin LONTCHI <benjamin.lontchi@groupe-esigelec.org>
2023-11-27 15:30:51 +0100

    :gem: modular training stage completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01__data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02__prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03__training import ModelTrainingPipeline
 
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data ingestion"
 try:
     logger.info(f">>>>> stage '{STAGE_NAME}' started <<<<<")
     prepare_base_model = DataIngestionTrainingPipeline()
@@ -19,6 +20,18 @@ try:
     logger.info(f"********************************")
     logger.info(f">>>>> stage '{STAGE_NAME}' started <<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>> stage '{STAGE_NAME}' completed <<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training model"
+try:
+    logger.info(f"********************************")
+    logger.info(f">>>>> stage '{STAGE_NAME}' started <<<<<")
+    prepare_base_model = ModelTrainingPipeline()
     prepare_base_model.main()
     logger.info(f">>>>> stage '{STAGE_NAME}' completed <<<<<\n\nx===========x")
 except Exception as e:
